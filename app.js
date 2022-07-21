4545dbc51fd3783a5d87a75260616c3d9da303f8
박가연 <gy.park@newen.co.kr>
2022-07-21 11:36:19 +0900

    댓글 목록 ui/ 읽은 상태 속성바꾸기

--- app.js
+++ app.js
@@ -1,73 +1,157 @@
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const content = document.createElement('div'); //
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; //@id값의 주소 
+//ul (디자인 ,아이콘, 폰트)
+
 
 //페이징구현 
 //페이지말고 다른 것도 생길수 있기 때문에 객체만듬
 const store = { //시작페이지 
   currentPage: 1,
+  feeds: [],
 };
 
 function getData(url) { //결괏값이 다르기 때문에 입력은 url이라는 인자로 받음
-
   ajax.open('GET', url, false); //method ,url ,boolean(false동기 방식)
   ajax.send();
 
   return JSON.parse(ajax.response);
 }
+
+function makeFeeds(feeds) {
+  for (let i = 0; i < feeds.length; i++) {
+    feeds[i].read = false;
+  }
+
+  return feeds;
+}
+
+
 //newsfeed 
 function newsFeed() {
-  const newsFeed = getData(NEWS_URL);
+  let newsFeed = store.feeds;
   const newsList = [];
-
-
-  //문자열로 하나의 틀을 만들어 놓고 데이터를 넣는 방식 
-  //복잡도를 줄일 수 있음 
   let template = `
-  <div class="container mx-auto p-4">
-    <h1>Hacker News</h1>
-    <ul>
-      {{__news_feed__}}
-    </ul>
-    <div>
-      <a href="#/page/{{__prev_page__}}">이전 페이지</a>
-      <a href="#/page/{{__next_page__}}">다음 페이지</a>
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
+        </div>
+      </div>
+      <div class="p-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
     </div>
-  </div>
-`;
-
-for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+  `;
+ 
+  if (newsFeed.length === 0) {
+    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+  }
+  
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-      <li>
-      <a href="#/show/${newsFeed[i].id}">
-          ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-      </li>
+      <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto">
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
+          </div>
+          <div class="text-center text-sm">
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+          </div>
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+          </div>  
+        </div>
+      </div>    
     `);
   }
   //replace 내용을 교체 
   template = template.replace('{{__news_feed__}}', newsList.join(''));
   template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
   template = template.replace('{{__next_page__}}', store.currentPage + 1);
-  
+
   container.innerHTML = template;
 }
 
-function newsDetail() { //이벤트 hashchange라는 함수 실행 
-  const id = location.hash.substr(7); //location 객체해시라는 속성으로 데이터 넘겨줌
-  //substr() 쓰고싶은 위치값
+function newsDetail() {
+  const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id))
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
-  //문자열 처리방식
-  container.innerHTML = `
-    <h1>${newsContent.title}</h1>
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
 
-    <div>
-       <a href="#/page/${store.currentPage}">목록으로</a>
+        {{__comments__}}
+
+      </div>
     </div>
   `;
+
+  for(let i=0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
+
+  function makeComment(comments, called = 0) {
+    const commentString = [];
+ 
+    for(let i = 0; i < comments.length; i++) {
+      commentString.push(`
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>      
+      `);
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
+    }
+
+    return commentString.join('');
+  }
+
+  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
 }
 
 //화면이 전환해야할때 판단하여 해당화는 화면으로 전환
@@ -89,7 +173,6 @@ function router() {
 }
 //화면의 전환 
 window.addEventListener('hashchange', router);
+//함수실행
 
-
-//함수 호출 
 router();
